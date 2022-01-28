30403286825cc5b9b4af321037212f28ccfb82d9
Hritik Kumar Singh <singhhritik978@gmail.com>
2022-01-28 23:10:35 +0530

    Added splash screen

--- app/src/main/java/com/debug_army/a10der/activities/SplashScreen.kt
+++ app/src/main/java/com/debug_army/a10der/activities/SplashScreen.kt
@@ -1,12 +1,22 @@
 package com.debug_army.a10der.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import com.debug_army.a10der.R
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        supportActionBar?.hide()
+        Handler(Looper.myLooper()!!).postDelayed({
+            val intent = Intent(this@SplashScreen, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
     }
 }
