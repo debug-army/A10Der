507225f047ec6647bae6bb1dcd25b99da4e9893a
sanmay321 <sanmaydas2002@gmail.com>
2022-10-10 22:51:12 +0530

    Added swipe library

--- app/src/main/java/com/debug_army/a10der/fragments/SwipeFragment.kt
+++ app/src/main/java/com/debug_army/a10der/fragments/SwipeFragment.kt
@@ -16,24 +16,29 @@ import com.lorentzos.flingswipe.SwipeFlingAdapterView
 class SwipeFragment : Fragment() {
 
     lateinit var binding: FragmentSwipeBinding
-    private var al = ArrayList<String>()
+    private var userList = ArrayList<String>()
     private var arrayAdapter: ArrayAdapter<String>? = null
-    private var i = 0
+//    private var i = 0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentSwipeBinding.inflate(layoutInflater)
+        return binding.root
+    }
 
-        al.add("a")
-        al.add("b")
-        al.add("c")
-        al.add("d")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        userList.add("a")
+        userList.add("b")
+        userList.add("c")
+        userList.add("d")
 
         //choose your favorite adapter
         arrayAdapter =
-            activity?.let { ArrayAdapter<String>(it, R.layout.item_view, R.id.helloText, al) }
+            activity?.let { ArrayAdapter<String>(it, R.layout.item_view, R.id.helloText, userList) }
 
         //set the listener and the adapter
         binding.frame.adapter = arrayAdapter
@@ -45,13 +50,13 @@ class SwipeFragment : Fragment() {
             override fun removeFirstObjectInAdapter() {
                 // this is the simplest way to delete an object from the Adapter (/AdapterView)
                 Log.d("LIST", "removed object!")
-                al.removeAt(0)
+                userList.removeAt(0)
                 arrayAdapter?.notifyDataSetChanged()
             }
 
             override fun onLeftCardExit(dataObject: Any) {
                 //Do something on the left!
-                //You also have access to the original object.
+                //You userListso have access to the originuserList object.
                 //If you want to use it just cast it (String) dataObject
                 Toast.makeText(activity, "left", Toast.LENGTH_SHORT).show()
             }
@@ -62,14 +67,13 @@ class SwipeFragment : Fragment() {
 
             override fun onAdapterAboutToEmpty(itemsInAdapter: Int) {
                 //for unlimited data
-//                al.add("XML $i")
+//                userList.add("XML $i")
 //                arrayAdapter?.notifyDataSetChanged()
 //                Log.d("LIST", "notified")
 //                i++
                 Toast.makeText(activity, "its over", Toast.LENGTH_SHORT).show()
             }
         })
-        return binding.root
     }
 
 }
